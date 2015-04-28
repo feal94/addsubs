c1326141e38a411f1e318a15eec111b758f2eeaf
brais.couce <brais.couce@udc.es>
2015-04-28 17:45:49 +0200

    Definidas las entidades persistentes (Job y Movie)

--- www/pint/addsubs/models.py
+++ www/pint/addsubs/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+	user = models.CharField(max_length=50)
+	id = models. AutoField
+	video = models.CharField(max_length=100)
+	language = models.CharField(max_length=100)
+	delay = models.IntegerField()
+	play = models.BooleanField()
+
+	def __unicode__(self):
+		return self.user + ": " + self.video
+
+
+class Movie(models.Model):
+	title = models.CharField(max_length=50)
+	director = models.CharField(max_length=50)
+	year = models.IntegerField()
+	hash = models.CharField(max_length=200)
+
+	def __unicode__(self):
+		return self.title + ": " + self.year
