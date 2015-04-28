b4981a7d6dccffb1220ce9d3298b4355e1c27d3d
brais.couce <brais.couce@udc.es>
2015-04-28 22:38:15 +0200

    - Añadido atributo finished a la entidad Job para saber si el trabajo terminó o no - En la entidad Job se ha puesto como clave foránea el atributo user a la entidad User que proporciona Django - En la entidad Job se ha puesto como clave foránea el atributo video a la entidad Movie - Corregidos comentarios en views.py

--- www/pint/addsubs/models.py
+++ www/pint/addsubs/models.py
@@ -1,14 +1,5 @@
 from django.db import models
-
-class Job(models.Model):
-	user = models.CharField(max_length=50)
-	video = models.CharField(max_length=100)
-	language = models.CharField(max_length=100)
-	delay = models.IntegerField()
-	play = models.BooleanField()
-
-	def __unicode__(self):
-		return self.user + ": " + self.video
+from django.contrib.auth.models import User
 
 
 class Movie(models.Model):
@@ -19,3 +10,15 @@ class Movie(models.Model):
 
 	def __unicode__(self):
 		return self.title + ": " + self.year
+
+
+class Job(models.Model):
+	user = models.ForeignKey(User)
+	video = models.ForeignKey(Movie)
+	language = models.CharField(max_length=100)
+	delay = models.IntegerField()
+	play = models.BooleanField()
+	finished = models.BooleanField()
+
+	def __unicode__(self):
+		return self.user + ": " + self.video
--- www/pint/addsubs/views.py
+++ www/pint/addsubs/views.py
@@ -1,19 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+
 def index(request):
 	if request.POST.has_key('ruta'):
 		#user=
 		path=request.POST['ruta']
-		# Aqui se podria comprobar con expresiones regulares si es una ruta
-		# Aqui comprobar si existe el video
+		# Comprobar con expresiones regulares si es una ruta
+		# Comprobar si existe el video
 		#video = algo sobre el path
 		if request.POST.has_key('idioma'):
 			language=request.POST['idioma']
-			# Aqui se podria comprobar con expresiones regulares si es una ruta
-			job=Job(user=user,video=video,language=language)
-			# Aqui se podria comprobar con expresiones regulares si es con el formato correcto
+			# Comprobar con expresiones regulares si es con el formato correcto
+			job=Job(user=user,video=video,language=language,finished=False)
 			job.save()
 	#job_list=Job.objects.all()
 	job_list = []
