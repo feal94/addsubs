80ad780c5950062877f04dedff0485cc2f5c5dbb
brais.couce <brais.couce@udc.es>
2015-04-28 15:59:31 +0200

    Añadida estructura básica del proyecto

---
+++ www/pint/pint/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'pint.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^index/', include('addsubs.urls')),
+    url(r'^$', include('addsubs.urls')),
+]
