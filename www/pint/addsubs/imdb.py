705fadb8e8a47322e087e0a58cc1b159e2cf80ca
xabi <xabier.costas@udc.es>
2015-05-28 18:50:20 +0200

    Concurrencia (sin probar la parte de la carpeta subtitles

--- www/pint/addsubs/imdb.py
+++ www/pint/addsubs/imdb.py
@@ -1,5 +1,7 @@
 import requests
 import urllib
+import Queue
+import threading
 
 class Movie():
 	def __init__(self, title, year, director):
@@ -9,29 +11,39 @@ class Movie():
 
 class Imdb():
 	server = 'http://www.omdbapi.com/?'
-
+	queue = Queue.Queue()
+	
 	def __init__(self, title):
 		self.title = title
 
 	def recoverInformation(self):
 		#method = 'GET'
 		#timeout = 500
+		print "Entrar entra, A ver"
 		url = self.server + 't=' + self.title + '&y=&plot=short&r=json'
 		try:
 			request = requests.get(url)
 			if request.status_code == 200:
 				answer = (request.text)
 				if answer["Response"] == "True": 
-					return answer
+					self.queue.put(answer)
+					return
 				else:
-					return "Not found"
+					self.queue.put("Not found")
+					return 
 			else:
-				return "Result failed"
+				self.queue.put("Result failed")
+				return 
 		except:
-			return "Server failed"
+			self.queue.put("Server failed")
+			return 
 
 	def main(self):
-		data = self.recoverInformation()
+		t = threading.Thread(target = self.recoverInformation)
+		t.start()
+		t.join()
+		data = self.queue.get()
+		#data = self.recoverInformation()
 		if data != "Server failed" and data != "Result failed" and data != "Not found": 
 			movie = Movie(data["Title"], data["Year"], director["Director"])
 			return movie
--- www/pint/addsubs/subtitles.py
+++ www/pint/addsubs/subtitles.py
@@ -5,11 +5,14 @@ import hashlib
 from imdb import Movie
 from imdb import Imdb
 #import eventlet
+import Queue
+import threading
 
 class MovieInformation():
 	#server = 'http://api.thesubdb.com/?action='
 	server = 'http://sandbox.thesubdb.com/?action='
-
+	queue = Queue.Queue()
+	
 	def __init__(self, name, lang):
 		self.language= lang
 		self.name = name
@@ -17,7 +20,8 @@ class MovieInformation():
 			'User-Agent': 'SubDB/1.0 (AddSubs/1.0;https://github.com/feal94/addsubs)'
 		}
 		self.hash= self.get_hash()
-
+		
+		
 	def get_hash(self):
 		readsize = 64 * 1024
 		with open(self.name, 'rb') as f:
@@ -37,11 +41,14 @@ class MovieInformation():
 		try:
 			request = requests.get(url, headers=self.user_agent)
 			if request.status_code == 200:
-				return request.text
+				self.queue.put(request.text)
+				return 
 			else:
-				return "Result failed"
+				self.queue.put("Result failed")
+				return 
 		except:
-			return "Server failed"
+			self.queue.put("Server failed")
+			return 
 
 
 	def download(self):
@@ -54,20 +61,30 @@ class MovieInformation():
 		try:
 			request = requests.get(url, headers=self.user_agent)
 			if request.status_code == 200:
-				return request.text
+				self.queue.put(request.text)
+				return  
 			elif request.status_code == 404:
-				return "Not avaliable"
+				self.queue.put("Not avaliable")
+				return  
 			elif request.status_code == 400:
-				return "Malformed request"
+				self.queue.put("Malformed request")
+				return  
 		except:
-			return "Server failed"
+			self.queue.put("Server failed")
+			return  
 
 
 	def main(self):
-		answer = self.check()
+		t = threading.Thread(target = self.check)
+		t.start()
+		t.join()
+		answer = self.queue.get()
 		if answer != "Result failed" and answer != "Server failed":
 			if self.language in answer:
-				subtitles = self.download()
+				t = threading.Thread(target = self.download)
+				t.start()
+				t.join()
+				subtitles = self.queue.get()
 				if subtitles != "Malformed request":
 					imdb = Imdb(self.name)  #aqui falta elmiminar antes la ruta
 					information = imdb.main()
